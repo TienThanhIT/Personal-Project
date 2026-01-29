ba8a9b6ec212e158776d5d3f03df18f3345badb0
Thanh Pham <phamtienthanh22@gmail.com>
2026-01-29 22:02:39 +0700

    deleted unused folder

---
+++ Personal-Project/LibMan/database.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE BookLib;
+USE BookLib;
+
+-- 1. Bảng lưu trữ thông tin sách
+CREATE TABLE sach (
+    book_id VARCHAR(50) PRIMARY KEY,
+    tieude VARCHAR(255) NOT NULL,
+    theloai VARCHAR(100),
+    tacgia VARCHAR(255),
+    tongso INT DEFAULT 0,
+    conlai INT DEFAULT 0,
+    nxb VARCHAR(255)
+);
+
+-- 2. Bảng lưu trữ thông tin độc giả
+CREATE TABLE docgia (
+    docgia_id INT PRIMARY KEY AUTO_INCREMENT, 
+    hoten VARCHAR(255) NOT NULL,
+    donvi VARCHAR(255),
+    sdt VARCHAR(11),
+    ngay_tao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 3. Bảng quản lý mượn trả
+CREATE TABLE phieu_muon (
+    phieu_id INT PRIMARY KEY AUTO_INCREMENT,
+    book_id VARCHAR(50),
+    docgia_id INT,
+    ngay_muon DATE,
+    han_tra DATE,
+    ngay_tra_thuc_te DATE DEFAULT NULL,
+    trang_thai VARCHAR(50) DEFAULT 'Dang muon', -- 'Dang muon', 'Da tra', 'Qua han'
+    FOREIGN KEY (book_id) REFERENCES sach(book_id),
+    FOREIGN KEY (docgia_id) REFERENCES docgia(docgia_id)
+);
